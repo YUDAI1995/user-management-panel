1f2bd9f8c061b6c0820fd7e383d7bfbb644322ad
yudaiy <y.yamamoto991103@gmail.com>
2021-12-30 19:01:01 +0900

    update a initial date

--- src/App.vue
+++ src/App.vue
@@ -63,18 +63,8 @@ export default defineComponent({
     this.userList = [
       {
         id: 1,
-        name: 'Leanne Graham',
-        email: 'Sincere@april.biz',
-      },
-      {
-        id: 2,
-        name: 'Ervin Howell',
-        email: 'Shanna@melissa.tv',
-      },
-      {
-        id: 3,
-        name: 'Clementine Bauch',
-        email: 'Nathan@yesenia.net',
+        name: 'John Smith',
+        email: 'sample01@sample.com',
       },
     ];
   },
